f143532b49b5e289c79108a3a1480f98a3df2955
darkxnovaz <DarKxNovaZ@gmail.com>
2020-03-23 15:33:26 +0200

    FINALIZED VERSION OF overlapPeriod.

--- facts.pl
+++ facts.pl
@@ -29,14 +29,15 @@ customerPreferredAccommodation(customer(ahmed, aly, 1993-01-30, single, 0, stude
 customerPreferredAccommodation(customer(mohamed, elkasad, 1999-01-30, single, 0, student), hotel, 100).
 
 
-overlapPeriod(period(YMD1 , YMD2), period(YMD1 , YMD2)).
-
+%OverlapPeriod method:
 overlapPeriod(period(YMD1,YMD2), period(YMD3,YMD4)):-  %2020 -> 2020 , 2019 -> 2021 ||| 2019 -> 2021 , 2020 -> 2020
 	(toString(YMD1,S1), parseInteger(S1,L1), nth0(0,L1,Year1), nth0(1,L1,Month1), nth0(2,L1,Day1)),
 	(toString(YMD2,S2), parseInteger(S2,L2), nth0(0,L2,Year2), nth0(1,L2,Month2), nth0(2,L2,Day2)), 
 	(toString(YMD3,S3), parseInteger(S3,L3), nth0(0,L3,Year3), nth0(1,L3,Month3), nth0(2,L3,Day3)),
 	(toString(YMD4,S4), parseInteger(S4,L4), nth0(0,L4,Year4), nth0(1,L4,Month4), nth0(2,L4,Day4)),
-	((Year3 >= Year1 , Year3 <  Year2 , (Year4 > Year3 ; (Year = )) ; (Year3 = Year2 , (Month3 < Month2 ; (Month3 = Month2 , Day3 =< Day2)))
+	P1 is Year1*10000 + Month1*100 + Day1, P2 is Year2*10000 + Month2*100 + Day2,
+	P3 is Year3*10000 + Month3*100 + Day3, P4 is Year4*10000 + Month4*100 + Day4, 
+	((P3 >= P1, P3 =< P2) ; (P4 =< P2 , P4 >= P1) ; (P3 =< P1, P1 =< P4) ; (P4 >= P2 , P2 >= P3)).
 
 toString(YMD , S) :-
 	with_output_to(atom(A), write(YMD)),
@@ -47,4 +48,4 @@ parseInteger([Head|Tail],[Head1|Tail1]):-
 	number_string(Head1,Head),
 	parseInteger(Tail,Tail1).
 
-	
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
