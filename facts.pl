90ed9817360043d19ae325db5bc382ffbaf12ac0
darkxnovaz <DarKxNovaZ@gmail.com>
2020-04-11 14:15:03 +0200

    testing choosepreferences

--- facts.pl
+++ facts.pl
@@ -55,26 +55,27 @@ subset([A|B] , [A|C]):-
 subset([_|A],B):-
 	subset(A,B).
 possibleSubset([H|T],O):-
-	subset([H|T],O1),
-	perm(O1,O).
+	(subset([H|T],O1),
+	perm(O1,O)).
 possibleSubset([],[]).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %ChoosePreferences
-activityChecker(Prefs):- member(activity(_) , Prefs) , \+(H = activity(_)).  
+add(X,List,[X|List]).
+activityChecker(Prefs):- member(activity(_) , Prefs).  
 choosePreferences([H|T] , ChosenPreferences) :-
     (H = activity(L),
     possibleSubset(L,O1),
     O = activity(O1),
-    possibleSubset(T,O2),
-    append([O], O2, ChosenPreferences)).
+    possibleSubset([O|T],ChoosePreferences).
 choosePreferences(Prefs ,  ChosenPreferences) :-
+    (nth0(0 , Prefs , H) , \+(H = activity(_)),
     activityChecker(Prefs),
-    select(activity(_) , Prefs , Prefsnew), %%removing the activity from the middle of the list.
     member(activity(Temp), Prefs),
-    append([activity(Temp)] , Prefsnew , Prefsupdated), %%adding it to the start of the list so it can be it's head.
+    delete(Prefs, activity(Temp) , Prefsnew), %%removing the activity from the middle of the list.
+   add(activity(Temp) , Prefsnew , Prefsupdated), %%adding it to the start of the list so it can be it's head.
     choosePreferences(Prefsupdated , ChosenPreferences). %%calling the first method on it again.
 choosePreferences(Prefs , ChosenPreferences) :-
-    \+ activityChecker(Prefs),
+    \+activityChecker(Prefs),
     possibleSubset(Prefs , ChosenPreferences).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %OverlapPeriod method:
